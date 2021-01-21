641acdd8ef62327fe941c156f7391cbc6857f918
Hypnootis <tm.pasanen@outlook.com>
2021-01-21 22:44:16 +0200

    Clicking now recognizes objects

--- src/classes.py
+++ src/classes.py
@@ -7,11 +7,11 @@ class Object():
         self.picture = picture
         self.inspect = inspect
         self.image = pygame.image.load("assets/sprites/" + self.picture)
-        self.rect = self.image.get_rect(center=(self.x, self.y))
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
         self.type = typeofobject
     def draw(self, surf):
         surf.blit(self.image, [self.x, self.y])
-    def inspect(self, inspect, surf):
+    def inspect(self, inspect, surf): #Probably something to do with the text function that this isn't working?
         text(self.surf, self.inspect, 12, (self.x, self.y - 50), pygame.Color("black"))
         
 
--- src/game.py
+++ src/game.py
@@ -31,15 +31,18 @@ while running:
                 changeRoom = True
             elif event.key == pygame.K_m:
                 playerStats["flags"]["puzzle1"] = True
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                for i in objects:
-                    if i.rect.collidepoint(event.pos) and i.type != "room":
-                        print("Clicked!")
-            elif event.button == 2:
-                for i in objects:
-                    if i.rect.collidepoint(event.pos) and i.type != "room":
-                        i.inspect()
+
+        #Combine these two, why are they even separated?
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            for i in objects:
+                if i.rect.collidepoint((event.pos)) and i.type != "room":
+                    print("Clicked on type " + i.type)
+                    break
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 2:
+            for i in objects:
+                if i.rect.collidepoint(event.pos) and i.type != "room":
+                    i.inspect() #Inspect no worky worky, make it worky worky
+                    break
     
  
     
